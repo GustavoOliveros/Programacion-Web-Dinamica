b5cd8e527a3b0eb8b07390cd0d2a3e1b00df0da1
GustavoOliveros <gusa05+1@gmail.com>
2022-08-27 02:01:10 -0300

    Modificaciones al tp 3

--- TP3/ej1/Vista/index.php
+++ TP3/ej1/Vista/index.php
@@ -1,71 +1,59 @@
 <!DOCTYPE html>
 <html lang="es">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta
-			name="viewport"
-			content="width=device-width, initial-scale=1.0" />
-		<title>Ejercicio 1</title>
-		<link
-			rel="stylesheet"
-			href="../../../bootstrap-5.2.0-dist/css/bootstrap.min.css" />
-	</head>
-	<body>
-		<div class="d-flex justify-content-center align-items-center vh-100">
-			<div
-				class="col-12 col-md-7 col-lg-5 col- bg-light shadow rounded p-3 border border-primary">
-				<form
-					action="subirArchivo.php"
-					method="post"
-					class="needs-validation"
-					enctype="multipart/form-data"
-					name="form"
-					id="form"
-					novalidate>
-					<div class="row">
-						<h1 class="text-center my-4"><strong>Archivos</strong></h1>
-						<div class="col-12 mb-4">
-							<label for="archivo" class="form-label"
-								>Ingrese un archivo .pdf, .doc o .docx (máximo 1.5MB):</label
-							>
-							<input
-								type="file"
-								class="form-control"
-								name="archivo"
-								id="archivo"
-								accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,.pdf,application/pdf"
-								required />
-							<div class="invalid-feedback">Obligatorio</div>
-						</div>
-						<div class="col-12 mb-3 d-flex justify-content-center">
-							<input type="submit" class="btn btn-primary" />
-						</div>
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../../../bootstrap-5.2.0-dist/css/bootstrap.min.css">
+	<title>Ejercicio 1 - Tp 3</title>
+</head>
+
+<body class="d-flex flex-column">
+	<?php
+	include_once "../../../Estructura/navbar.php";
+	?>
+	<div class="d-flex justify-content-center align-items-center vh-100">
+		<div class="col-12 col-md-7 col-lg-5 col- bg-light shadow rounded p-3 border border-primary">
+			<form action="subirArchivo.php" method="post" class="needs-validation" enctype="multipart/form-data" name="form" id="form" novalidate>
+				<div class="row">
+					<h1 class="text-center my-4"><strong>Archivos</strong></h1>
+					<div class="col-12 mb-4">
+						<label for="archivo" class="form-label">Ingrese un archivo .pdf, .doc o .docx (máximo 2MB):</label>
+						<input type="file" class="form-control" name="archivo" id="archivo" accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,.pdf,application/pdf" required />
+						<div class="invalid-feedback">Obligatorio</div>
 					</div>
-				</form>
-			</div>
+					<div class="col-12 mb-3 d-flex justify-content-center">
+						<input type="submit" class="btn btn-primary" />
+					</div>
+				</div>
+			</form>
 		</div>
+	</div>
+	<?php
+	include_once "../../../Estructura/footer.php";
+	?>
+	<script src="../../../bootstrap-5.2.0-dist/js/bootstrap.min.js"></script>}
+	<script>
+		(() => {
+			"use strict";
+			const forms = document.querySelectorAll(".needs-validation");
+			Array.from(forms).forEach((form) => {
+				form.addEventListener(
+					"submit",
+					(event) => {
+						if (!form.checkValidity()) {
+							event.preventDefault();
+							event.stopPropagation();
+						}
 
-		<script src="../../../bootstrap-5.2.0-dist/js/bootstrap.min.js"></script>
-		<script>
-			(() => {
-				"use strict";
-				const forms = document.querySelectorAll(".needs-validation");
-				Array.from(forms).forEach((form) => {
-					form.addEventListener(
-						"submit",
-						(event) => {
-							if (!form.checkValidity()) {
-								event.preventDefault();
-								event.stopPropagation();
-							}
+						form.classList.add("was-validated");
+					},
+					false
+				);
+			});
+		})();
+	</script>
+</body>
 
-							form.classList.add("was-validated");
-						},
-						false
-					);
-				});
-			})();
-		</script>
-	</body>
 </html>
--- TP3/ej1/Vista/subirArchivo.php
+++ TP3/ej1/Vista/subirArchivo.php
@@ -2,61 +2,82 @@
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Archivo</title>
-    <link rel="stylesheet" href="../../../bootstrap-5.2.0-dist/css/bootstrap.min.css" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../bootstrap-5.2.0-dist/css/bootstrap.min.css">
+    <title>Ejercicio 1 - Tp 3</title>
 </head>
 
-<body>
-    <div class="col-12 col-md-7 alert alert-danger m-3 p-3 mx-auto">
+<body class="d-flex flex-column min-vh-100">
+    <?php
+    include_once "../../../Estructura/navbar.php";
+    ?>
 
+    <?php
+    // directorio
+    $dir = "../Archivos/";
 
-        <?php
-        // directorio
-        $dir = "/archivos/";
+    // Variables
+    $esValido = false;
+    $i = 0;
 
-        // Variables
-        $esValido = false;
-        $i = 0;
+    // formatos aceptados
+    $formatosValidos = ["application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/pdf"];
 
-        // formatos aceptados
-        $formatosValidos = ["application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/pdf"];
+    if ($_FILES['archivo']['error'] <= 0) {
 
-        if ($_FILES['archivo']['error'] <= 0) {
-
-            // tipos validos doc y pdf
-            while ($i < count($formatosValidos) && !$esValido) {
-                if ($formatosValidos[$i] == $_FILES['archivo']['type']) {
-                    $esValido = true;
-                }
-                $i++;
+        // tipos validos doc y pdf
+        while ($i < count($formatosValidos) && !$esValido) {
+            if ($formatosValidos[$i] == $_FILES['archivo']['type']) {
+                $esValido = true;
             }
-
-            if ($esValido) {
-                // tamaño max 2mb
-                if (($_FILES['archivo']['size'] / 1000) / 1000 > 1.5) {
-                    // guardar el archivo y mensaje de exito con un link al mismo
-                    echo 'EXITO: <a href="'. $_FILES['archivo']. '">Descarge su archivo aquí</a>';
-                }else{
-                    // mensaje de error por archivo muy grande
-                    echo "El archivo es demasiado grande.";
-                }
-            }else{
-                // mensaje de error por tipo de archivo no valido
-                echo "Tipo de archivo inválido: El archivo debe ser .docx, .doc o .pdf.";
-            }
-        } else {
-            echo "Ocurrió un error en la subida del archivo.";
+            $i++;
         }
 
-        ?>
-        <br>
-        <a href="ej1.html">Volver a inicio</a>
-    </div>
+        if ($esValido) {
+            // tamaño max 2mb
 
+            if ((($_FILES['archivo']['size'] / 1024) / 1024) < 2) {
+                // guardar el archivo y mensaje de exito con un link al mismo
+                if(!copy($_FILES['archivo']['tmp_name'], $dir . $_FILES['archivo']['name'])){
+                    echo '<div class="col-12 col-md-7 alert alert-danger m-3 p-3 mx-auto">
+                    Hubo un error al guardar el archivo.
+                    </div>';
+                }else{
+                    echo '<div class="col-12 col-md-7 alert alert-success m-3 p-3 mx-auto">
+                    <h1>¡Felicidades!</h1><a href="' . $dir . $_FILES['archivo']['name'] . '" download="' . $_FILES['archivo']['name'] .
+                        '">Descarge su archivo aquí</a>
+                    </div>';
+                }
+            } else {
+                // mensaje de error por archivo muy grande
+                echo '<div class="col-12 col-md-7 alert alert-danger m-3 p-3 mx-auto">
+                El archivo es demasiado grande. Debe ser menor a 2MB
+                </div>';
+            }
+        } else {
+            // mensaje de error por tipo de archivo no valido
+            echo '<div class="col-12 col-md-7 alert alert-danger m-3 p-3 mx-auto">
+            Tipo de archivo inválido: El archivo debe ser .docx, .doc o .pdf.
+            </div>';
+        }
+    } else {
+        echo '<div class="col-12 col-md-7 alert alert-danger m-3 p-3 mx-auto">
+            Ocurrió un error en la subida del archivo
+            </div>';
+    }
+
+    ?>
+
+
+    <?php
+    include_once "../../../Estructura/footer.php";
+    ?>
     <script src="../../../bootstrap-5.2.0-dist/js/bootstrap.min.js"></script>
+    <script>
+        document.querySelector(".alert").innerHTML += '<br><br><a href="index.php">Volver</a>';
+    </script>
 </body>
 
 </html>
