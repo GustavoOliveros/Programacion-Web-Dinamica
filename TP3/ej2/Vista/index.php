483a5a225fdf62208dd3aa8bb7872d8dc9ca7e24
GustavoOliveros <gusa05+1@gmail.com>
2022-08-27 00:03:06 -0300

    TP 3

---
+++ TP3/ej2/Vista/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suibir archivo txt</title>
+</head>
+<body>
+
+<form action="../control/subirArchivo.php" method="POST" enctype="multipart/form-data">
+
+
+<input name="miarchivo" id="miarchivo" type="file"  />
+
+<input value="Enviar" type="submit">
+
+
+
+</form>
+    
+</body>
+</html>
