f458c5981608ced6a70dc909675f1ede55d60c27
GustavoOliveros <gusa05+1@gmail.com>
2022-09-23 23:21:07 -0300

    Mensaje no necesario eliminado

--- Vista/TP4/CambioDuenio.php
+++ Vista/TP4/CambioDuenio.php
@@ -1,15 +1,4 @@
 <?php
-// Ejercicio 6 –
-
-// Crear una página “CambioDuenio.php” que contenga un formulario en donde se solicite el
-// numero de patente de un auto y un numero de documento de una persona - LISTO
-
-// estos datos deberán ser enviados a una página “accionCambioDuenio.php” 
-// en donde se realice cambio del dueño del auto de la patente ingresada en el formulario.
-
-// Mostrar mensajes de error en caso de que el auto o la persona no se encuentren
-// cargados. Utilizar css y validaciones javaScript cuando crea conveniente. Recordar usar la capa de control
-// antes generada, no se puede acceder directamente a las clases del ORM.
 
 // Encabezado
 $titulo = "Cambio de Dueño - TP 4";
